3e5b58ab35d618d9672fd478eced45088ef05383
Neamul09 <neamulmorshed@gmail.com>
2026-05-30 17:47:30 +0600

    Fix saved shelf drawer slide action on mobile and wrap items in anchor links

--- app.js
+++ app.js
@@ -622,11 +622,13 @@ function renderDrawerItems() {
     const gradient = getGradientForCategory(book.category);
     html += `
       <div class="fav-item">
-        <div class="fav-item-cover-mini ${gradient}"></div>
-        <div class="fav-item-details">
-          <h5 class="fav-item-title">${book.title}</h5>
-          <span class="fav-item-author">${book.author}</span>
-        </div>
+        <a href="${book.url}" target="_blank" rel="noopener noreferrer" class="fav-item-link">
+          <div class="fav-item-cover-mini ${gradient}"></div>
+          <div class="fav-item-details">
+            <h5 class="fav-item-title">${book.title}</h5>
+            <span class="fav-item-author">${book.author}</span>
+          </div>
+        </a>
         <div class="fav-item-remove-btn" onclick="toggleFavorite('${book.id}')" title="Remove from shelf">
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-2"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/><line x1="10" x2="10" y1="11" y2="17"/><line x1="14" x2="14" y1="11" y2="17"/></svg>
         </div>
